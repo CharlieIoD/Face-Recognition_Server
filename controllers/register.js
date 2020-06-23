3187bb69a995f04527ad6b08efbedf804b787dc5
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-23 19:51:38 +0100

    778 became 646

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,4 @@
-const handleRegister = ((req, res, db, bcrypt) => {
+const handleRegister = (req, res, db, bcrypt) => {
     const {email, name, password} = req.body;
     if (!email || !name || !password) {
         return res.status(400).json('Incorrect form submission');
@@ -27,9 +27,9 @@ const handleRegister = ((req, res, db, bcrypt) => {
             .catch(trx.rollback);
     })
        
-        .catch(err => res.status(400).json('Registration not permitted'))
-})
+        .catch(err => res.status(400).json('Registration not permitted'));
+}
 
 module.exports = {
-    handleRegister
-}
+    handleRegister: handleRegister
+};
